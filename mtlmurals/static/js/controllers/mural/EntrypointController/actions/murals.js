2860fd2472954740dcaa5009645868f34c4cfb13
Morgan Aubert <morgan.aubert@zoho.com>
2017-01-25 23:02:08 -0500

    Ensured 'request' action type is dispatched when fetching data

--- mtlmurals/static/js/controllers/mural/EntrypointController/actions/murals.js
+++ mtlmurals/static/js/controllers/mural/EntrypointController/actions/murals.js
@@ -6,6 +6,7 @@ import ActionTypes from '../constants/ActionTypes';
 export function fetchMurals() {
   return async (dispatch, getState) => {
     try {
+      dispatch({ type: ActionTypes.MURALS_FETCH_REQUEST, murals });
       let url = window.Urls['api:v1:mural:list']();
       const murals = (await axios.get(url)).data;
       dispatch({ type: ActionTypes.MURALS_FETCH_SUCCESS, murals });
