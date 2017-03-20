bf3ab5eb943b600372fa1f63183fbd7ed8864ed1
Morgan Aubert <morgan.aubert@zoho.com>
2017-03-19 21:56:27 -0400

    Updated JS tests

--- tests/jstests/core/DOMRouter-test.js
+++ tests/jstests/core/DOMRouter-test.js
@@ -9,4 +9,20 @@ describe('DOMRouter', () => {
     router.execAction('test', 'init');
     expect(testController.init).toHaveBeenCalled();
   });
+
+  test('can trigger the right controller using data attributes on the <body> tag', () => {
+    let testController01 = { init: jest.fn(), doSomething: jest.fn(), doSomethingElse: jest.fn(), };
+    let testController02 = { init: jest.fn(), doSomethingElseAgain: jest.fn(), };
+    let controllers = { test01: testController01, test02: testController02, };
+    let router = new DOMRouter(controllers);
+    // Set up our document body
+    document.body.setAttribute('data-controller', 'test01');
+    document.body.setAttribute('data-action', 'doSomething');
+    router.init();
+    expect(testController01.init).toHaveBeenCalled();
+    expect(testController01.doSomething).toHaveBeenCalled();
+    expect(testController01.doSomethingElse).not.toHaveBeenCalled();
+    expect(testController02.init).not.toHaveBeenCalled();
+    expect(testController02.doSomethingElseAgain).not.toHaveBeenCalled();
+  });
 });
